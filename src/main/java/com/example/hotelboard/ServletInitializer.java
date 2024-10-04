bbba3c0574efd295cc49f1091b4379c2be7eb0fc
horang52 <167264939+horang0210@users.noreply.github.com>
2024-10-04 09:37:29 +0900

    react 연결

---
+++ src/main/java/com/example/hotelboard/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.example.hotelboard;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HotelBoardApplication.class);  // 애플리케이션의 메인 클래스 이름
+    }
+}
---
+++ src/main/java/com/example/hotelboard/service/ReservationServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.hotelboard.service;
+
+public class ReservationServiceImpl {
+}
